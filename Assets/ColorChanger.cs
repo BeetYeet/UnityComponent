eee86a562531521edbe1360c97414d68c1b4cf83
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2018-10-01 08:53:56 +0200

    Made color ocillate

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+
+	public SpriteRenderer sr;
+	public float changeTime = 10;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		sr.color = new Color(.5f, Mathf.Sin(Time.time/changeTime), .5f);
+	}
+}
