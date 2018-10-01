1379061dcb03de1949a971d6112eac811790a75a
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2018-10-01 10:20:32 +0200

    Organisation

---
+++ Assets/Scripts/CarScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour {
+
+	public float rotateSpeed = 1f;
+	public float driveSpeed = 1f;
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+		if ( Input.GetKey( KeyCode.LeftArrow ) )
+		{
+			transform.Rotate( new Vector3( 0f, 0f, 1f ), rotateSpeed * 90 * Time.deltaTime );
+		}
+		if ( Input.GetKey( KeyCode.RightArrow ) )
+		{
+			transform.Rotate( new Vector3( 0f, 0f, 1f ), rotateSpeed * -90 * Time.deltaTime );
+		}
+		if ( Input.GetKey( KeyCode.UpArrow ) )
+		{
+			transform.Translate( 0f, driveSpeed * 1f * Time.deltaTime, 0f, Space.Self );
+		}
+		if ( Input.GetKey( KeyCode.DownArrow ) )
+		{
+			transform.Translate( 0f, driveSpeed * -0.3f * Time.deltaTime, 0f, Space.Self );
+		}
+
+	}
+}
